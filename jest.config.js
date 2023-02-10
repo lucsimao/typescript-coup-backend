a472ec94c7e9369c4c7cddcbffdb93383f4858b4
lucsimao <lucasimao64@gmail.com>
2023-02-10 00:24:17 -0300

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,18 @@
-const { resolve } = require("path");
+const { resolve } = require('path');
 const root = resolve(__dirname);
 module.exports = {
   rootDir: root,
-  displayName: "unit-tests",
-  testMatch: ["<rootDir>/src/**/*.spec.ts"],
-  testEnvironment: "node",
+  displayName: 'unit-tests',
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  testEnvironment: 'node',
   clearMocks: true,
-  preset: "ts-jest",
-  setupFilesAfterEnv: ["./jest.setup.ts"],
-  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.spec.ts"],
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.spec.ts',
+    '!src/**/mocks/*.ts',
+  ],
   coverageThreshold: {
     global: {
       branches: 90,
